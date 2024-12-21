dc9360f1a38dfee0b806a8abd8726a7d02c618e2
cyan0515 <ballpen517@yahoo.co.jp>
2024-12-22 00:28:10 +0900

    delete: overflow_if_mul as you should use  instead

--- my-library/overflow_if_mul.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// a * b がオーバーフローするならtrueを返す
-// keywords: オーバーフロー オーバフロー
-template <class T> bool overflow_if_mul(const T& a, const T& b) {
-  return (std::numeric_limits<T>::max() / a) < b;
-}
