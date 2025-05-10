b7add65fcc93979db7dff85758fbb000c86c2ee2
cyan0515 <ballpen517@yahoo.co.jp>
2025-05-11 02:26:35 +0900

    perf: enhance combination O(n^2) to O(n)

--- my-library/math/combination.cpp
+++ my-library/math/combination.cpp
@@ -1,24 +1,37 @@
 #include <bits/stdc++.h>
+#include <atcoder/modint>
 using namespace std;
+using mint = atcoder::modint998244353;
 
-/**
- * @fn
- * k = 0,1,...,n について nCk を計算して返す。
- * ret[i][j] = iCj の値。
- * 計算量 O(N^2)
- * @param n 総数
- * @param mod 法
- */
-vector<vector<long long>> comb(const int& n, const int& mod) {
-  vector<vector<long long>> v(n + 1,vector<long long>(n + 1, 0));
-  for (int i = 0; i < v.size(); i++) {
-    v[i][0] = 1;
-    v[i][i] = 1;
+// https://drken1215.hatenablog.com/entry/2018/06/08/210000
+vector<mint> fac, finv, inv;
+
+/// nCk 計算のための前計算処理
+/// 計算量は O(n)
+void init_comb(int n, int mod) {
+  fac.resize(n);
+  finv.resize(n);
+  inv.resize(n);
+  fac[0] = fac[1] = 1;
+  finv[0] = finv[1] = 1;
+  inv[1] = 1;
+  for (int i = 2; i < n; i++) {
+    fac[i] = fac[i - 1] * i;
+    inv[i] = mod - inv[mod%i] * (mod / i);
+    finv[i] = finv[i - 1] * inv[i];
   }
-  for (int j = 1; j < v.size(); j++) {
-    for (int k = 1; k < j; k++) {
-      v[j][k] = (v[j - 1][k - 1] + v[j - 1][k])%mod;
-    }
-  }
-  return v;
+}
+
+/// nCk を計算して返す。
+/// 計算量は O(1)
+mint comb(int n, int k) {
+  if (n < k) return 0;
+  if (n < 0 || k < 0) return 0;
+  return fac[n] * finv[k] * finv[n - k];
+}
+
+/// nHk を計算して返す。
+/// 計算量は O(1)
+mint hom(int n, int k) {
+  return comb(n+k-1, k);
 }
