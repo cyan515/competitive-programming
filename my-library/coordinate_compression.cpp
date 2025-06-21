b1cadbdbc58bcaf44b8a5c047ac4c2055fef8c1c
cyan0515 <ballpen517@yahoo.co.jp>
2025-06-21 23:46:57 +0900

    feat: add size and decompress on coodinate_compression

--- my-library/coordinate_compression.cpp
+++ my-library/coordinate_compression.cpp
@@ -17,9 +17,28 @@ public:
    * 
    * @param val 圧縮前の値
    */
-  long long compress(const long long& val) const {
+  int compress(const long long& val) const {
     long long ret = lower_bound(_sorted.begin(),_sorted.end(),val) - _sorted.begin();
     return ret;
+  }
+  
+  /**
+   * @fn
+   * 受け取った値を座標圧縮する前にいくつだったかを返す。
+   * 計算量 O(1)
+   * 
+   * @param val 圧縮後の値
+   */
+  long long decompress(const int& val) const {
+    return this->_sorted.at(val);
+  }
+  
+  /**
+   * @fn
+   * 圧縮後の値の種類数を返す。
+   */
+  size_t size() const {
+    return this->_sorted.size();
   }
   
 private:
