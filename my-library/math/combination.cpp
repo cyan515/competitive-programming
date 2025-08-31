8350ce3a57d53ac62fb9744b136f6292029a6c11
cyan0515 <ballpen517@yahoo.co.jp>
2025-08-31 12:20:46 +0900

    refactor: use vector::at for safer access

--- my-library/math/combination.cpp
+++ my-library/math/combination.cpp
@@ -12,13 +12,13 @@ void init_comb(int n, int mod) {
   fac.resize(n);
   finv.resize(n);
   inv.resize(n);
-  fac[0] = fac[1] = 1;
-  finv[0] = finv[1] = 1;
-  inv[1] = 1;
+  fac.at(0) = fac.at(1) = 1;
+  finv.at(0) = finv.at(1) = 1;
+  inv.at(1) = 1;
   for (int i = 2; i < n; i++) {
-    fac[i] = fac[i - 1] * i;
-    inv[i] = mod - inv[mod%i] * (mod / i);
-    finv[i] = finv[i - 1] * inv[i];
+    fac.at(i) = fac.at(i-1) * i;
+    inv.at(i) = mod - inv.at(mod%i) * (mod / i);
+    finv.at(i) = finv.at(i-1) * inv.at(i);
   }
 }
 
@@ -27,7 +27,7 @@ void init_comb(int n, int mod) {
 mint comb(int n, int k) {
   if (n < k) return 0;
   if (n < 0 || k < 0) return 0;
-  return fac[n] * finv[k] * finv[n - k];
+  return fac.at(n) * finv.at(k) * finv.at(n-k);
 }
 
 /// nHk を計算して返す。
